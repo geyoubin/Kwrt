f79db246784692f4bd7e1e92891db29cdea8862b
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2025-01-11 11:01:18 +0800

    update

--- devices/qualcommax_ipq60xx/diy.sh
+++ devices/qualcommax_ipq60xx/diy.sh
@@ -11,15 +11,13 @@ wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.
 wget -N https://raw.githubusercontent.com/openwrt/openwrt/refs/heads/openwrt-24.10/package/base-files/files/sbin/sysupgrade -P package/base-files/files/sbin/
 wget -N https://github.com/openwrt/openwrt/raw/refs/heads/openwrt-24.10/package/network/config/firewall/Makefile -P package/network/config/firewall/
 
-git clone https://github.com/LiBwrt/nss-packages package/feeds/nss-packages
+git clone https://github.com/LiBwrt/nss-packages package/feeds/kiddin9/nss-packages
 
 rm -rf feeds/kiddin9/shortcut-fe feeds/kiddin9/fibocom_QMI_WWAN feeds/kiddin9/quectel_QMI_WWAN feeds/kiddin9/xtables-wgobfs feeds/kiddin9/fullconenat-nft/
 
 rm -rf package/feeds/packages/ovpn-dco package/feeds/packages/xr_usb_serial_common package/feeds/packages/openvswitch package/feeds/packages/xtables-addons
 
 rm -rf package/libs/libnftnl/patches/001-libnftnl-add-fullcone-expression-support.patch package/network/config/firewall4/patches/001-firewall4-Add-fullcone-support.patch
-
-sed -i "/nss_packages/d" feeds.conf.default
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += kmod-qca-mcs kmod-qca-nss-drv-igs kmod-qca-nss-drv-l2tpv2 kmod-qca-nss-drv-lag-mgr kmod-qca-nss-drv-map-t kmod-qca-nss-drv-pppoe kmod-qca-nss-drv-pptp kmod-qca-nss-drv-qdisc kmod-qca-nss-macsec/' target/linux/qualcommax/Makefile
 
